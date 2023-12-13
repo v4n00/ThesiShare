3fca3c93c7291f8dfacbcc682fef44a2405d575f
v4n00 <105587619+v4n00@users.noreply.github.com>
2023-12-13 22:49:58 +0200

    Added warning to add .env file

--- backend/index.js
+++ backend/index.js
@@ -14,11 +14,18 @@ app.use(
 	})
 );
 
+// warning to add .env file
+if (!process.env.PORT) {
+	console.error('ERROR: You are missing the .env file\nRefer to example.env for an example configuration');
+	process.exit(1);
+}
+
+// set up everything
 initDB();
 app.use('/api', resetDB);
 app.use('/api', accountsRoutes);
 
 // start the server
-let port = process.env.PORT || 8000;
+let port = process.env.PORT;
 app.listen(port);
 console.log(`Backend is running at http://localhost:${port}/`);
